a024b6503bf247b48f056ef73b6549cf12852f5a
syedjunaid106 <junaidas106@gmail.com>
2025-05-07 15:29:02 +0500

    Initial commit

--- src/app/menu/[category]/page.js
+++ src/app/menu/[category]/page.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import menuData from '@/data/menuData';
 import MenuList from '@/components/MenuList';
-
+import { useRouter } from 'next/navigation';
 const CategoryPage = () => {
   const router = useRouter();
   const { category } = router.query;
